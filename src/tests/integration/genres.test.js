5913a1d49f6949bbb7a7d35eccd6cc8ffe74a974
shujashah <shuja.ashraf11@gmail.com>
2024-03-19 15:58:41 +0530

    integration testing for genres

--- src/tests/integration/genres.test.js
+++ src/tests/integration/genres.test.js
@@ -1,18 +1,26 @@
 const request = require('supertest');
+const { Genre } = require('../../models/entities/genre');
 
 let server;
 describe('/api/genres', () => {
-  // in integration testing we should load the server before and close the server after each test
   beforeEach(() => {
     server = require('../../../index');
   });
-  afterEach(() => {
-    server.close(); // shut down the server
+  afterEach(async () => {
+    await server.close();
+    await Genre.deleteMany({});
   });
+
   describe('GET /', () => {
     it('should return all genres', async () => {
+      const genres = [{ name: 'genre1' }, { name: 'genre2' }];
+
+      await Genre.collection.insertMany(genres);
       const res = await request(server).get('/api/genres');
       expect(res.status).toBe(201);
+      expect(res.body.genres.length).toBe(2);
+      expect(res.body.genres.some((g) => g.name === 'genre1')).toBeTruthy();
+      expect(res.body.genres.some((g) => g.name === 'genre2')).toBeTruthy();
     });
   });
 });
